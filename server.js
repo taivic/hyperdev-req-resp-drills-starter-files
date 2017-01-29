0a38900b8e7cce1348457bc74a499d5889cbcbef
taivic <vicky.yen.tai@gmail.com>
2017-01-29 13:49:16 -0600

    initial commit

--- server.js
+++ server.js
@@ -4,10 +4,24 @@
 // ==================================
 
 const express = require('express');
+const bodyParser = require('body-parser')
 const app = express();
 
 // your code here. 
+const madLib = (params) => {
+	const params = {adjective1, adjective2, adjective3, adverb, name, pronoun, noun, place};
+	return (
+		`There's a ${adjective1} new ${name} in ${place} and ${pronoun} has everybody talking. 
+		Stunningly ${adjective2} and ${adverb} ${adjective3}, all the cool kids know it. 
+  		However, ${name} has a secret - ${name}'s a vile vampire.  
+  		Will it end with a bite, or with a stake through the ${noun}?`);
+	
+};
 
+// GET requests to the root of the server 
+app.get('/', (req, res) =>
+		res.json(madLib(params)); 
+})
 
 // listen for requests :)
 app.listen(process.env.PORT || 8080, () => console.log(
